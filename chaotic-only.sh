a57c26823cd28a0d91852a661a19d968aebe4064
Taylor Whitesides <tbwhitesides@gmail.com>
2023-03-12 21:06:43 -0600

    Create chaotic-only.sh
    This is from a Reddit request for somebody who only wanted the shell file for chaotic.

---
+++ chaotic-only.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Disable SteamOS ReadOnly
+
+sudo steamos-readonly disable
+
+#Make pacman actually work by making it a full Arch Distro.
+#This makes it work for limited distro stuff, but we want full capabilities:
+
+#Initiate pacman-keys
+sudo pacman-key --init
+
+#Populate the new linux system
+sudo pacman-key --populate archlinux
+
+#Make pacman work for just about anything else too by utilizing chaotic-aur. Don’t install too much though.
+
+#Debug (before or after?)
+
+grep -qxF '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf || echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n' | sudo tee -a /etc/pacman.conf
+
+#First, install the primary key - it can then be used to install our keyring and mirrorlist.
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+grep -qxF '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf || echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n' | sudo tee -a /etc/pacman.conf
+
+#Proper update to get the repository into pacman.
+
+if sudo pacman -Syy; then
+
+echo "Success: Updated pacman repository successfully."
+
+else
+
+echo "Success: Already Updated."
